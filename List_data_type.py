1378ca689e02be0d5d609182275f0d6e5a0f584d
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-04 22:59:20 +0530

    Practice 4 May

--- List_data_type.py
+++ List_data_type.py
@@ -1,1 +1,44 @@
 # List Data Type
+# list_one = [1,2,3,'d','t']  ordered set of element(any data type)/char(strin)
+# list_two = [1,2,3,["Gangster"],'d','t']
+# list_three = list((1,2,3,'d','t'))
+# print(list_one[-1])
+# print(list_one[1])
+# print(list_one[0::2])
+# print(list_one[-4:-1:2])
+# # list digit is included
+#
+# # print(list_two)
+# # print(list_three)
+
+
+a = ['a', 'b', 'c', 0, 1, 2, 3, 3]
+a[0] = 'b'
+
+print(a)
+
+# b = ['a','b','c']
+#
+# # b = [4,5,6,'d','e','f']
+
+# a.extend('x')
+# # # print(a)
+# # # print(a.append('x'))
+# # print(a)
+# a.pop()
+# print(a)
+# print(a)
+# a.remove([1,2])
+# print(a)
+# print(a.count('a'))
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+# b.sort(reverse=True)
+# print(b)
+# a.reverse()
+# print(a)
+
+# print(min(a))
+# print(max(a))
+# print(sum(a))
