973f4127a69014071faa772a81fb0e6463914ae0
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-05 16:03:40 +0530

    Add files via upload
    Practice 5 May

---
+++ Boolean_Datatype.py
@@ -0,0 +1,49 @@
+# print(type(True))
+# False
+
+# Comparison Operators
+# a =1
+# b = 3
+# # print(a==b)
+# #
+# print(f'b!=a is {b!=a}')
+# print(f'a>b  is {a>b}')
+# print(f'b<a  is {b<a}')
+# print(f'b>=a is {b>=a}')
+# print(f'b>=a is {b>=a}')
+
+# Keyword Operators
+
+# and  or        Logical Operators
+# c = b<a and b>=a and (b<a or b!=a)
+#
+# d = b<a or b!=a
+# print(c)
+
+
+# is,is not       Identity operators
+#
+# a = 5
+# b = 5
+# print(a is b)
+# print(id(a))
+# print(id(b))
+# a = [1,2,3]
+# b = [1,2,3]
+# print(a is not b)
+# print(id(a))
+# print(id(b))
+
+# in,in not       Membership operators
+
+quote = 'ors are special symbols in Python that carry out arithmetic or logical computation. The value that the operator operates on is called the op'
+word = 'symbols'
+
+print(f'{word} exist in quote {word in quote}')
+
+b = [4, 5, 6, 'd', 'e', 'f']
+num = 65
+print(num not in b)
+
+set_a = {'A', 'B', 'C'}
+
