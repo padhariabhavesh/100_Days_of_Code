6619f77955ecdd16ab26ba7aec0c20417b6a0fb7
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-05 19:35:05 +0530

    Add files via upload
    Practice 5 May

---
+++ Loops.py
@@ -0,0 +1,135 @@
+# Loops
+# Nested loops
+# Tuple unpacking using for loops
+
+# a = [1,2,3,4,5,6,7,8,9,10,12,13]
+#
+# for var_a in a:
+#  print(f'I am runing for:{var_a}')
+#  print(f'I am runing for:{var_a}')
+#  print(f'I am runing for:{var_a}')
+#  print(f'I am runing for:{var_a}')
+#  print(f'I am runing for:{var_a}')
+#  print(f'I am runing for:{var_a}')
+#  print(f'I am runing for:{var_a}')
+
+
+# for i in range(11):
+#  print(f'i is now {i}')
+
+# cupboard = ['shirt','pant','chaddi','bunyan','shoes']
+#
+# num = len(cupboard)
+# print(num)
+#
+# for i in range(len(cupboard)):
+#  print(cupboard[i])
+#
+# print('x'*20)
+#
+# # for item in cupboard:
+# #    print(item+'This is cool')
+
+
+# for i in range(0,10):
+#  print('x' * 20)
+#  print(f'This is table of {i}')
+#  print('x'*20)
+#  for j in range(0, 10):
+#     print(f'{j} multiplied by {i} is equal to {i*j}')
+
+# Examples
+
+# numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# sum = 0
+# for i in range(100):
+#  sum = sum + i
+# print(f'Sum of list is {sum}')
+#
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+#
+# for i in adj:
+#  for j in fruits:
+#     print(i,j)
+
+# for i in range(10):
+#  print(i)
+# else:
+#  print('List is finished')
+# #
+# fruits = ["apple", "banana", "cherry"]
+# adj = ["red", "big", "tasty"]
+#
+# for x,y in zip(fruits,adj):
+#  pass
+#  print(x,y)
+
+# for i in fruits:
+#  print(i)
+# else:
+#  print('I ate all the frits')
+
+
+# D = {'name': 'Bob', 'age': 25}
+#
+# for key, value in D.items():
+#  print(key,value)
+#
+
+# D.items()
+# D.keys()
+# D.values()
+
+D = {
+	'key1': 'This is values for key1',
+	'key2': 'This is values for key2',
+	'key3': 'This is values for key3',
+	'key4': 'This is values for key4',
+	'key5': 'This is values for key5',
+	'key6': 'This is values for key6',
+	'key7': 'This is values for key7',
+}
+
+# l = list(D.keys())
+#
+# reversed_key = sorted(l,reverse=True)
+# print(reversed_key)
+#
+# for key in reversed_key:
+#  val = D[key]
+#  print(val)
+#
+# for val in D.values():
+#  print(val)
+#
+# for key in D.keys():
+#  print(key)
+#
+#
+# for key in D:
+#  print(D[key])
+#
+# T = [(1, 2), (3, 4), (5, 6)]
+#
+# x = tuple(list(range(10)))
+# print(x)
+#
+# for i in x:
+#  print(i)
+# #
+# # for a,b in T:
+# #    print(a,b)
+
+
+smoothie_fruits = ("Pineapple", "orange", "banana", "apple")
+# #
+# # for i in smoothie_fruits:
+# #    print(i)
+
+
+for index, item in enumerate(smoothie_fruits):
+ print(index,item)
+
