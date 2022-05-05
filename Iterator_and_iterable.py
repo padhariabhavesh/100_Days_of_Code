6201e0aa79385bc318f157456b0f63eeca51e8ae
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-05 18:13:31 +0530

    Add files via upload
    Practice May 5

---
+++ Iterator_and_iterable.py
@@ -0,0 +1,21 @@
+
+# Iterator and Iterable
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12]
+
+for var_a in a:
+	print(var_a)
+print('x' * 20)
+for var_a in iter(a):
+	print(var_a)
+
+# var_a = iter(a)
+# print(next(var_a))
+# print(next(var_a))
+# print(next(var_a))
+# print(next(var_a))
+# print(next(var_a))
+# print(next(var_a))
+# print(next(var_a))
+# print(next(var_a))
+# print(next(var_a))
+# print(next(var_a))
