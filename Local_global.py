6534c7d4e1a0862809d420620d6798edfa2970ec
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-10 17:14:42 +0530

    Practice 10 May

---
+++ Local_global.py
@@ -0,0 +1,14 @@
+# Global Space
+# def washingmachine():
+#  # Enclosing function
+#
+#  clothes = 'Dry and Clean'
+#  def dryclean():
+#     # Nested Function
+#     nonlocal clothes
+#     clothes = 'Dry and Clean'
+#     print(clothes)
+#  dryclean()
+#  print(clothes)
+#
+# washingmachine()
