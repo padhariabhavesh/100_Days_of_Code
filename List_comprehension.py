e1134b533bee524d5a95d1131406b475c0f2f480
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-16 21:48:03 +0530

    Practice 16 May

---
+++ List_comprehension.py
@@ -0,0 +1,45 @@
+# # List Comprehension
+#
+# num = [1,2,3,4,5,6,7,8,9]
+# sqr_num1 = []
+# for x in num:
+#  sqr_num1.append(x**2)
+# print(sqr_num1)
+#
+# print('x'*20)
+#
+#
+# sqr_num2 = [x**2 for x in num]
+#
+#
+#
+# print(sqr_num2)
+
+
+# name = []
+# for letter in 'Mohsin':
+#  name.append(letter)
+# print(name)
+
+# name = [letter for letter in 'Mohsin']
+#
+# print(name)
+
+# Conditional In list Compreehension
+
+
+# even = []
+#
+# for x in range(10):
+#  if x%2==0:
+#     even.append(x)
+#
+# print(even)
+
+# List COmp with conditional
+
+# even = [x**2 for x in range(10) if x%2==0]
+#
+#
+#
+# print(even)
