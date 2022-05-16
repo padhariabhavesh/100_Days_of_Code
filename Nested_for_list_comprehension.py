0818b231aebc50e1fb6292c2844d0b3980506efb
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-16 21:54:34 +0530

    Practice 16 May

---
+++ Nested_for_list_comprehension.py
@@ -0,0 +1,15 @@
+# Nested For in List Comprehension
+#
+# my_list = []
+#
+# for x in[2, 4, 6]:
+#  for y in [1, 3, 5]:
+#     my_list.append(x*y)
+#
+# print(my_list)
+#
+#
+#
+#
+# my_list = [x*y for x in[2, 4, 6] for y in [1, 3, 5] if x*y !=10 if x*y !=20 ]
+# print(my_list)
