3bdbdfded436ec6eeee9b5917e8968b5c0491327
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-16 17:26:20 +0530

    Practice 16 May

---
+++ StdLibraries.py
@@ -0,0 +1,16 @@
+# Built Heart
+from turtle import *
+color('red','yellow')
+begin_fill()
+left(140)
+forward(111.65)
+for i in range(200):
+   right(1)
+   forward(1)
+left(120)
+for i in range(200):
+   right(1)
+   forward(1)
+forward(111.65)
+end_fill()
+done()
