d0a4d3a73ac33e7a13f5c20ead1981e885fcdc74
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-18 18:00:04 +0530

    Practice 18 May

---
+++ Bank Project.py
@@ -0,0 +1,40 @@
+# Bank Project
+# Method mega brain eater and skull crawler exercise
+
+#
+# # Methods Mega Brain Eater and Skull Crawler Exercise
+# # Khali Bank Ltd Exercise 1 and 1.1
+# class Account():
+#  def __init__(self,_name,__balance):
+#     self._name = _name
+#     self.__balance = __balance
+#     print(f'Account created for {_name}')
+#
+#  def deposit(self,amount):
+#     if amount > 0:
+#        self.__balance += amount
+#        self.show()
+#
+#  def withdraw(self,amount):
+#     # 0 < amount <= self.__balance
+#     if amount > 0 and self.__balance >= amount:
+#        self.__balance -= amount
+#        self.show()
+#     else:
+#        print(f'Paon Poan {self._name}, You are Bankcurrup')
+#
+#  def show(self):
+#     print(f'{self._name} __balance is {self.__balance}')
+#
+#
+# Mohsin  = Account('Mohsin', 10)
+# # Mohsin.show()
+# Mohsin.deposit(10)
+# # Mohsin.show()
+#
+# John  = Account('John', 100)
+# # John.show()
+# John.deposit(10)
+# John.withdraw(120)
+# # John.show()
+
