967e43c6c46d6d03390f495a6a84c9fbaeb3f6f6
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2022-05-18 15:36:44 +0530

    Practice 18 May

---
+++ OOP.py
@@ -0,0 +1,45 @@
+# class map():
+#  material = 'house'
+#  print(f'I build {material}')
+
+# house1 = map()
+#
+# house2 = map()
+# house3 = map()
+# house1.material = 'new_house'
+# house1.material2 = 'Very Good house'
+# print(house1.material2)
+
+# house2.material = 'new_house'
+# print(house2.material)
+#
+# house3.material = 'new_house'
+# print(house3.material)
+
+
+# class map():
+#  material = 'house'
+#  print(f'I build {material}')
+#  def adder(self):
+#     material = 'house'
+#     x = 2+2
+#     return x
+# house = map()
+# print(house.material)
+# print(house.adder())
+# Class Summary
+
+"""
+
+1- Class  : Template for creating objects,
+All Objects Created from same class will have same attributes
+
+2- Object : Is Instance of a Class
+
+3- Instentiate : Process of Creating Instance
+
+4- Method: A function Defined in Class
+
+5- Attribute = A Variable bound to an instance of a class
+
+"""
